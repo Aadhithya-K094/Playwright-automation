2362d23a10c8a9549f3474cca19542660cea8df8
Aadhithya-K094 <aadhiaadhi094@gmail.com>
2026-07-01 12:45:36 +0530

    files was modified

--- playwrigth_testing/tests/EMIS_API.spec.js
+++ playwrigth_testing/tests/EMIS_API.spec.js
@@ -2,7 +2,7 @@ const { test, expect } = require("@playwright/test");
 
 // //get
 // test("login api", async ({ request }) => {
-//   const api = await request.get("");
+//   const api = await request.get("https://eeqzof4a8g.execute-api.ap-south-1.amazonaws.com/api/oauth_test");
 //   console.log(api.json());
 //   await expect(api.status()).toBe(200);
 // });
@@ -10,7 +10,7 @@ const { test, expect } = require("@playwright/test");
 //api/emislogin-post
 test("api/emislogin", async ({ request }) => {
   const apipost = await request.post(
-    "https://tng2c2.tnschools.gov.in/emis_login/api/emislogin",
+    "https://emislogin.tnschools.gov.in/emis_login/api/testlogin",
     {
       data: {
         "emis_username": "4028609",
