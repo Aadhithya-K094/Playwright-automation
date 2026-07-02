fd8b9f1b9ca4ca9b0460b627e1fc02faeb2a2f65
Aadhithya-K094 <aadhiaadhi094@gmail.com>
2026-07-02 09:29:35 +0530

    files was modified

--- tests/TNEMISAPI.spec.js
+++ tests/TNEMISAPI.spec.js
@@ -1,26 +1,27 @@
 const { test, expect } = require("@playwright/test");
 
+
 //api/getUsrIdByDes?usrTpe=5&usrTpe1=37&dis=&blk=
-test.only('api/getUsrIdByDes?usrTpe=5&usrTpe1=37&dis=&blk=', async ({ request }) => {
-    const api = await request.get('/emis3APICode/api/getUsrIdByDes?usrTpe=5&usrTpe1=37&dis=&blk=');
-    console.log(api.json());
-    await expect(api.status()).toBe(200);
+test('api/getUsrIdByDes?usrTpe=5&usrTpe1=37&dis=&blk=', async ({ request }) => {
+    const response = await request.get('https://webapistage.tnsed.com/api/tnemis/getUsrIdByDes?usrTpe=5&usrTpe1=37&dis=&blk=1');
+    console.log(await response.json());
+    await expect(response.status()).toBe(200);
 });
 
 
 //api/GetOldPassword?oldPass=test@123
-test.only('//api/GetOldPassword?oldPass=test@123', async ({ request }) => {
-    const api1 = await request.get('emis3APICode/api/GetOldPassword?oldPass=test@123');
-    console.log(api1.json());
-    await expect(api1.status()).toBe(200);
+test('//api/GetOldPassword?oldPass=test@123', async ({ request }) => {
+    const response1 = await request.get('https://webapistage.tnsed.com/api/tnemis/GetOldPassword?oldPass=test@123');
+    console.log(await response1.json());
+    await expect(response1.status()).toBe(200);
 });
 
 
 //api/getUserNotifications
-test.only('//api/getUserNotifications', async ({ request }) => {
-    const api2 = await request.get('/emis3APICode/api/getUserNotifications');
-    console.log(api2.json());
-    await expect(api2.status()).toBe(200);
+test('//api/getUserNotifications', async ({ request }) => {
+    const response2 = await request.get('https://webapistage.tnsed.com/api/tnemis/getUserNotifications');
+    console.log(await response2.json());
+    await expect(response2.status()).toBe(200);
 });
 
 
@@ -30,7 +31,7 @@ test.only('//api/getUserNotifications', async ({ request }) => {
 
 //api/DeptBaseManageGet
 test("//api/DeptBaseManageGet", async ({ request }) => {
-    const apipost = await request.post("/emis3APICode/api/DeptBaseManageGet",
+    const responsepost = await request.post("https://webapistage.tnsed.com/api/tnemis/DeptBaseManageGet",
         {
             data: {
 
@@ -41,109 +42,111 @@ test("//api/DeptBaseManageGet", async ({ request }) => {
             headers: { accept: "application/json" },
         },
     );
-    console.log(apipost.json());
-    await expect(apipost.status()).toBe(200);
+    console.log(await responsepost.json());
+    await expect(responsepost.status()).toBe(200);
 });
 
 
 //api/postLoginLog
 test("//api/postLoginLog", async ({ request }) => {
-    const apipost1 = await request.post("/emis3APICode/api/postLoginLog",
+    const responsepost1 = await request.post("https://webapistage.tnsed.com/api/tnemis/postLoginLog",
         {
             data: {
             },
             headers: { accept: "application/json" },
         },
     );
-    console.log(apipost1.json());
-    await expect(apipost1.status()).toBe(200);
+    console.log(await responsepost1.json());
+    await expect(responsepost1.status()).toBe(200);
 });
 
 
 //api/OtpUpdateNewPass
 test("//api/OtpUpdateNewPass", async ({ request }) => {
-    const apipost3 = await request.post("/emis3APICode/api/OtpUpdateNewPass",
+    const responsepost3 = await request.post("https://webapistage.tnsed.com/api/tnemis/OtpUpdateNewPass",
         {
             data: {
                 "triggered_by_username": "4028609",
-                "otp_purpose_id": 6, 
+                "otp_purpose_id": 6,
                 "otp": "55555"
             },
             headers: { accept: "application/json" },
         },
     );
-    console.log(apipost3.json());
-    await expect(apipost3.status()).toBe(200);
+    console.log(await responsepost3.json());
+    await expect(responsepost3.status()).toBe(200);
 });
 
 
 //api/OtpPasswordReset
 test("//api/OtpPasswordReset", async ({ request }) => {
-    const apipost4 = await request.post("/emis3APICode/api/OtpPasswordReset",
+    const responsepost4 = await request.post("https://webapistage.tnsed.com/api/tnemis/OtpPasswordReset",
         {
             data: {
             },
             headers: { accept: "application/json" },
         },
     );
-    console.log(apipost4.json());
-    await expect(apipost4.status()).toBe(200);
+    console.log(await responsepost4.json());
+    await expect(responsepost4.status()).toBe(200);
 });
 
 
 //api/UpdateUserPassword
 test("api/UpdateUserPassword", async ({ request }) => {
-    const apipost5 = await request.post("/emis3APICode/api/UpdateUserPassword",
+    const responsepost5 = await request.post("https://webapistage.tnsed.com/api/tnemis/UpdateUserPassword",
         {
             data: {
             },
             headers: { accept: "application/json" },
         },
     );
-    console.log(apipost5.json());
-    await expect(apipost5.status()).toBe(200);
+    console.log(await responsepost5.json());
+    await expect(responsepost5.status()).toBe(200);
 });
 
 
 //api/OtpPasswordResetSub
 test("api/OtpPasswordResetSub", async ({ request }) => {
-    const apipost6 = await request.post("/emis3APICode/api/OtpPasswordResetSub",
+    const responsepost6 = await request.post("https://webapistage.tnsed.com/api/tnemis/OtpPasswordResetSub",
         {
             data: {
             },
             headers: { accept: "application/json" },
         },
     );
-    console.log(apipost6.json());
-    await expect(apipost6.status()).toBe(200);
+    console.log(await responsepost6.json());
+    await expect(responsepost6.status()).toBe(200);
 });
 
 
 //api/emislogout
 test("api/emislogout", async ({ request }) => {
-    const apipost7 = await request.post("/emis_login/api/emislogout",
+    const responsepost7 = await request.post("https://webapistage.tnsed.com/api/tnemis/emislogout",
         {
             data: {
             },
             headers: { accept: "application/json" },
         },
     );
-    console.log(apipost7.json());
-    await expect(apipost7.status()).toBe(200);
+    console.log(await responsepost7.json());
+    await expect(responsepost7.status()).toBe(200);
 });
 
 
 //api/testnewlogin
 test("api/testnewlogin", async ({ request }) => {
-    const apipost8 = await request.post("/emis_login/api/testnewlogin",
+    const responsepost8 = await request.post("https://webapistage.tnsed.com/api/tnemis/testnewlogin",
         {
             data: {
+                "emis_username": "4028617",
+                "emis_password" : "test@123"
             },
             headers: { accept: "application/json" },
         },
     );
-    console.log(apipost8.json());
-    await expect(apipost8.status()).toBe(200);
+    console.log(await responsepost8.json());
+    await expect(responsepost8.status()).toBe(200);
 });
 
 
