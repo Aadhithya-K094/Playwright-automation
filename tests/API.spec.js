3c604201ee023303b1952b05e44ed7086f0de540
Aadhithya-K094 <aadhiaadhi094@gmail.com>
2026-07-02 11:55:35 +0530

    files was modified

--- tests/API.spec.js
+++ tests/API.spec.js
@@ -156,3 +156,16 @@ test("/api/save_recommendations", async ({ request }) => {
   console.log(await responsepost8.json());
   expect(responsepost8.status()).toBe(200);
 });
+
+
+
+
+
+///api/RCDistList
+test("/api/RCDistList", async ({ request }) => {
+  const response1 = await request.get(
+    "https://tng2c1.tnschools.gov.in/api/RCDistList",
+  );
+  console.log(await response1.json());
+  expect(response1.status()).toBe(200);
+});
