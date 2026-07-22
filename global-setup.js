8957ac09a5e47b693929f889d80975d608efa659
Aadhithya-K094 <aadhiaadhi094@gmail.com>
2026-07-22 12:51:47 +0530

    new file are added and modified

---
+++ global-setup.js
@@ -0,0 +1,12 @@
+ 
+// export default globalSetup;
+export default async function globalSetup() {
+
+    console.log("================================");
+    console.log("Global Setup Started");
+     console.log(" Playwright Automation Started");
+    console.log(" Environment : Stage");
+    console.log(" Browser : Chromium");
+    console.log("================================");
+
+}
